4283c21a69cdcf174d64b7b7bcab2534f8acb782
Stephen McMillen <Shtevetm@gmail.com>
2019-10-09 22:47:38 -0500

    switch statement and return are functioning properly. Need to dry up case statements and get 'quantity ordered MINUS stock' arithmetic working.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -32,16 +32,16 @@ start();
 
 
 // ===============================FUNCTIONS============================= //
-// Test Connection //
-function testConnection() {
-    connection.query("SHOW tables", function (err, res) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("connection successful");
-        }
-    })
-} connection.end()
+// // Test Connection //
+// function testConnection() {
+//     connection.query("SHOW tables", function (err, res) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log("connection successful");
+//         }
+//     })
+// } connection.end()
 
 
 // Start Function //
@@ -53,11 +53,21 @@ function start() {
         }
         else {
             console.log("SUCCESSFULLY CONNECTED TO DB");
-            // For loop to list items //
+            // For loop to display items //
             for (var i = 0; i < res.length; i++) {
-                var idArray = "ID: " + res[i].item_id + " - " + res[i].product_name + ": $" + res[i].price;
+                var idArray = (chalk.bold.blue("ID: ")) + (chalk.bold.yellow(res[i].item_id)) +
+                    " - " + res[i].department_name +
+                    " - " + res[i].product_name +
+                    ": $" + (chalk.bold.green(res[i].price));
+
+                // Log array of items //
                 console.log(idArray);
             }
+            console.log(chalk.bold.blue("==============================="));
+            // Create table // - SCRAPPED
+            // console.table(res); - SCRAPPED
+
+
             // Input prompt for user to select item/quantity //
             inquirer.prompt([{
                 type: "input",
@@ -69,17 +79,95 @@ function start() {
                 name: "quantity",
                 message: "Please enter the quantity of  you would like to purchase"
             }])
-                .then(function (answer) {
-                    console.log(chalk.bold.green("Product: ") + answer.item +
-                        (chalk.bold.yellow("\nQuantity: ")) + answer.quantity);
+                // ==========Switch Statement for different ID selections =========
+                .then(function (answer1) {
+                    switch (answer1.item) {
+                        case "1":
+                            //======== If item orderes < stock quantity, console log ID, Item, Quantity, and "PURCHASE COMPLETE"
+                            if (answer1.quantity <= res[0].stock_quantity) {
+                                console.log(chalk.bold.green("ID: ") + answer1.item +
+                                    (chalk.bold.yellow("\nProduct: ")) + res[0].product_name +
+                                    (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity + (chalk.bold.blue("\nPURCHASE COMPLETE! \nYou will receive an email when your items are shipped")));
+                                //***NOT FUNCTIONAL*** =============== Reduce quantity entered from stock quantity ======================== //
+                                connection.query("SELECT stock_quantity - \"answer1.quantity\" FROM products WHERE item_id=1", function (err, result) {
+                                    if (err) 
+                                        throw err;
+                                })
+                            }
+                            // =========== If quantity ordered > stock, Console log "We only have XX in stock" =============== //
+                            else {
+                                console.log(
+                                chalk.bold.yellow("\nYikes! We only have " + res[0].stock_quantity + " " + res[0].product_name + "s in stock!") + 
+                                (chalk.bold.blue("\nYou'll need to adjust your quantity")));
+                                connection.end();
+                                break;
+                            }
+                            connection.end();
+                            break;
 
-                    // confirm //
-                    inquirer.prompt([{
-                        type: "confirm",
-                        name: "confirm",
-                        message: "confirm?"
-                    }])
-                })
+
+                        // ============= Additional Cases - NEED TO DRY UP ================
+
+                        // case "2":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[1].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "3":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[2].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "4":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[3].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "5":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[4].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "6":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[5].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "7":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[6].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "8":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[7].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "9":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[8].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+
+                        // case "10":
+                        //     console.log(chalk.bold.green("ID: ") + answer1.item +
+                        //         (chalk.bold.yellow("\nProduct: ")) + res[9].product_name +
+                        //         (chalk.bold.yellow("\nQuantity: ")) + answer1.quantity);
+                        //     break;
+                        // =============== Default if incorrect input is entered, console log message ============== //
+                        default:
+                            console.log((chalk.bold.yellow("Check your input! " + (chalk.bold.blue(answer1.item)) + " is not a valid ID")));
+                            connection.end();
+                            break;
+                    }
+                });
         }
-    })
+    });
 }
--- bamazondb.sql
+++ bamazondb.sql
@@ -3,7 +3,7 @@
 USE bamazonDB;
 
 
--- ### Create table ###
+		### Create table ###
 -- CREATE TABLE products (
 --     item_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 --     product_name VARCHAR(30) NOT NULL,
@@ -12,17 +12,25 @@ USE bamazonDB;
 --     stock_quantity INT(4)
 -- );
 
-INSERT INTO products
-    (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
-    ("test item 1", "department 1", 1, 9),
-    ("test item 2", "department 2", 22, 88),
-    ("test item 3", "department 3", 333, 777)
-;
+
+    ("Sushi Knife", "Kitchen", 25, 200),
+	("Cutting Board", "Kitchen", 15, 300),
+    ("Binoculars", "Outdoors", 30, 100),
+    ("Tent", "Outdoors", 80, 200),
+    ("Compass", "Outdoors", 20, 400),
+    ("Office Chair", "Office", 100, 100),
+	("Desk", "Office", 100, 200),
+    ("Keyboard", "Office", 20, 1000),
+    ("Drum Set", "Music", 600, 100),
+    ("Guitar", "Music", 90, 200);
+    
 
 -- ### purge test items from table ###
 -- DELETE FROM products WHERE item_id BETWEEN 1 and 3;
 
 
-SELECT *
-from products;
+SELECT * FROM products;
+SHOW TABLES;
+DESCRIBE products;
